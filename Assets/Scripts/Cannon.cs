234561a51831f97a2c883d88c4365b2dd8f3b0ac
Squame4 <squame4@gmail.com>
2021-03-28 18:15:00 +0200

    Aggiunto script del cannone

---
+++ Assets/Scripts/Cannon.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannon : MonoBehaviour
+{
+    private Transform baseRotonda;
+    private Transform canna;
+    
+    void Start()
+    {
+        //Ruotarli sull'asse Z
+        baseRotonda = this.gameObject.transform.GetChild(0).GetChild(1);
+        canna = baseRotonda.GetChild(0).GetChild(2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //baseRotonda.Rotate(0,0,1);
+        //canna.Rotate(0,0,1);
+    }
+}
