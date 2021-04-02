8b3dfb797e7bc6f2e4003a15ed25290e24b2250a
Squame4 <squame4@gmail.com>
2021-04-02 11:19:11 +0200

    Aggiunta animazione per il blocco bonus

--- Assets/Scripts/BonusGearActivator.cs
+++ Assets/Scripts/BonusGearActivator.cs
@@ -8,12 +8,13 @@ public class BonusGearActivator : MonoBehaviour
     [SerializeField] private GameObject particlePrefab;
     private GameObject _bonusGear;
     private GameObject _particle;
-    public Transform bloccoPadre;
+    public Animation anim;
     public float timeStay = 5f;
     public bool firstTime = true;
     void Start()
     {
-        bloccoPadre = this.gameObject.transform.GetComponentInParent<Transform>();
+        anim = gameObject.GetComponentInParent(typeof(Animation)) as Animation;
+
     }
 
     // Update is called once per frame
@@ -22,6 +23,7 @@ public class BonusGearActivator : MonoBehaviour
         
     }
     void OnTriggerEnter(Collider other) {
+        anim.Play("BonusBlockActivation");
         if(firstTime){
             PlayerController player = other.GetComponent<PlayerController>();
             if (player != null) {
@@ -33,6 +35,7 @@ public class BonusGearActivator : MonoBehaviour
     }
 
     public void spawnBonusGears(){
+        
         _particle = Instantiate(particlePrefab) as GameObject;
         _particle.transform.position = this.gameObject.transform.position;
         for (int i = 0; i < this.gameObject.transform.childCount; i++) {
