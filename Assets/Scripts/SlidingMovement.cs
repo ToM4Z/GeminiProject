c7dca9f59b6e5f36e71493574ffde7b3abf43ed2
Gianfranco Sapia <43842080+Sfrizz97@users.noreply.github.com>
2021-04-15 19:47:40 +0200

    Platform finished

--- Assets/Scripts/SlidingMovement.cs
+++ Assets/Scripts/SlidingMovement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class SlidingMovement : MonoBehaviour
@@ -17,47 +18,76 @@ public class SlidingMovement : MonoBehaviour
     private Vector3 start_position;
     private LeftOrRight platformDirection = LeftOrRight.Default;
     public Type type;
-    public CharacterController Player;
-    private bool moveWithPlatform;
-    private bool showPlatform;
+    public CharacterController PlayerController;
+    private bool playerOnPlatform;
+    private bool triggerPopUp;
+    private float TimePassed;
+    public MeshCollider childWithColl;
+    public bool platformActivated;
 
     // Start is called before the first frame update
     void Start()
     {
         this.platformDirection = LeftOrRight.Right;
         this.start_position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
-        this.showPlatform = true;
+        this.TimePassed = 0;
+        this.platformActivated = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(this.type == Type.Slider) {
-            transform.position += transform.forward * (int)platformDirection * speed * Time.deltaTime;
-            if(moveWithPlatform) {
-                Player.Move(transform.forward * (int)platformDirection * speed * Time.deltaTime);
+        if(platformActivated) {
+            if(this.type == Type.Slider) {
+                transform.position += transform.forward * (int)platformDirection * speed * Time.deltaTime;
+                if(playerOnPlatform) {
+                    PlayerController.Move(transform.forward * (int)platformDirection * speed * Time.deltaTime);
+                }
+            } else if (this.type == Type.Elevator) {
+                transform.position += transform.up * (int)platformDirection * speed * Time.deltaTime;
+                if(playerOnPlatform) {
+                    PlayerController.Move(transform.up * (int)platformDirection * speed * Time.deltaTime);
+                }
+            } else {
+                if (triggerPopUp) {
+                    this.TimePassed += Time.deltaTime;
+                    this.transform.Rotate((int)platformDirection * speed * 7 * Time.deltaTime,0,0);
+                }
+                if(playerOnPlatform) {
+                }
+                if(TimePassed >= 2.0f) {
+                    this.childWithColl.enabled = false;
+                    Vector3 scale = new Vector3(-1,-1,-1);
+                    if(this.transform.localScale.x > 0.1f) {
+                        this.transform.localScale += scale * speed * Time.deltaTime;
+                    } else {
+                        this.transform.localScale = Vector3.zero;
+                    }
+                    StartCoroutine(PopUp());
+                }
+                if(this.childWithColl.enabled) {
+                    if(this.transform.localScale.x != 1.0f) {    
+                        Vector3 scale = new Vector3(1,1,1);
+                        if(this.transform.localScale.x < 0.9f) {
+                            Quaternion rot = new Quaternion();
+                            rot.eulerAngles = new Vector3(0,this.transform.rotation.eulerAngles.y,0);
+                            this.transform.rotation = rot;
+                            this.transform.localScale += scale * speed * Time.deltaTime;
+                            this.triggerPopUp = false;
+                        }
+                        else if(this.transform.localScale.x > 1.0f) {
+                            this.transform.localScale = scale;
+                        }
+                    } 
+                }
             }
-        } else if (this.type == Type.Elevator) {
-            transform.position += transform.up * (int)platformDirection * speed * Time.deltaTime;
-            if(moveWithPlatform) {
-                Player.Move(transform.up * (int)platformDirection * speed * Time.deltaTime);
+            if(Math.Abs((transform.position - start_position).magnitude) >= distance) {
+                SwitchDirection();
             }
-        } else
-        {
-            StartCoroutine(PopUp());
+            if(transform.rotation.eulerAngles.x >= distance && transform.rotation.eulerAngles.x <= (360-distance)) {
+                SwitchDirection();
+            }
         }
-        if((transform.position - start_position).magnitude >= distance) {
-            SwitchDirection();
-        } else if ((transform.position - start_position).magnitude <= -distance ) {
-            SwitchDirection();
-        }
-        /*if(moveWithPlatform) {
-            //Player.transform.position += transform.forward * (int)platformDirection * speed * Time.deltaTime;
-            Player.Move(transform.forward * (int)platformDirection * speed * Time.deltaTime);
-            //Debug.Log(Player.transform.position);
-            //Debug.Log(Player.transform.position+= transform.forward * (int)platformDirection * speed * Time.deltaTime);
-            //Debug.Log("-------------------------------");
-        }*/
     }
 
     private void SwitchDirection() {
@@ -71,36 +101,42 @@ public class SlidingMovement : MonoBehaviour
     }
 
     void OnTriggerEnter(Collider coll) {
-        Player = coll.GetComponent<CharacterController>();
-        if (Player != null ){
-            moveWithPlatform = true;
-            Debug.Log("Player sulla piattaforma");
+        if (PlayerController == null) {
+            PlayerController = coll.GetComponent<CharacterController>();
+            playerOnPlatform = true;
         }
-
+        if (this.type == Type.PopUp) {
+            this.triggerPopUp = true;
+        }
     }
 
     void OnTriggerExit(Collider coll) {
-        moveWithPlatform = false;
-        Player = null;
-        Debug.Log(Player);
+        if (PlayerController == coll.GetComponent<CharacterController>()) {
+            playerOnPlatform = false;
+            PlayerController = null;
+        }
+        if(this.type == Type.PopUp) {
+            this.TimePassed = 0.0f;
+        }
+    }
+
+    private IEnumerator Shake(){
+        yield return new WaitForSeconds(3.0f);
     }
 
     private IEnumerator PopUp(){
-        if(showPlatform) {
-            BoxCollider[] myColliders = gameObject.GetComponents<BoxCollider>();
-            yield return new WaitForSeconds(3.0f);
-            this.showPlatform = false;
-            foreach(BoxCollider bc in myColliders) bc.enabled = false;
-            //gameObject.GetComponent<BoxCollider>().enabled = false;
-            Debug.Log("Not visibile");
-        } else {
-            BoxCollider[] myColliders = gameObject.GetComponents<BoxCollider>();
-            yield return new WaitForSeconds(3.0f);
-            this.showPlatform = true;
-            foreach(BoxCollider bc in myColliders) bc.enabled = true;
-            //gameObject.GetComponent<BoxCollider>().enabled = true;
-            Debug.Log("Now visibile");
-        }
-            
+        yield return new WaitForSeconds(3.0f);
+        this.childWithColl.enabled = true;
+        this.TimePassed = 0.0f;
     }
+
+    public void SetPlatformActivated(bool activated) {
+        this.platformActivated = activated;
+    }
+
+    public bool GetPlatformActivated() {
+        return this.platformActivated;
+    }
+
+
 }
