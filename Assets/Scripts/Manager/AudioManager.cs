6cb09cc0754e81c855152718102be92da20da3fa
Thomas <thom_96@hotmail.it>
2021-05-29 15:58:20 +0100

    Improving audio managers (wik)

--- Assets/Scripts/Manager/AudioManager.cs
+++ Assets/Scripts/Manager/AudioManager.cs
@@ -6,10 +6,70 @@ public class AudioManager : MonoBehaviour, IGameManager
 {
     public ManagerStatus status { get; private set; }
 
+    [SerializeField] private AudioSource soundSource;
+    [SerializeField] private AudioSource musicSource;
+
 
     public void Startup()
     {
+        musicSource.ignoreListenerVolume = true;
+        musicSource.ignoreListenerPause = true;
+
+        soundVolume = 1f;
+        musicVolume = 1f;
+
         status = ManagerStatus.Started;
+    }
+
+
+    public float soundVolume
+    {
+        get { return AudioListener.volume; }
+        set { AudioListener.volume = value; }
+    }
+
+    public bool soundMute
+    {
+        get { return AudioListener.pause; }
+        set { AudioListener.pause = value; }
+    }
+
+    public float musicVolume
+    {
+        get { return musicSource.volume; }
+        set { if (musicSource != null) musicSource.volume = value; }
+    }
+
+    public bool musicMute
+    {
+        get
+        {
+            if (musicSource != null)
+                return musicSource.mute;
+            return false;
+        }
+        set { if (musicSource != null) musicSource.mute = value; }
+    }
+
+    public void PlaySound(AudioClip clip)
+    {
+        soundSource.PlayOneShot(clip);
+    }
+
+    public void PlayLevelMusic()
+    {
+        //PlayMusic((AudioClip)Resources.Load("Music/?"));
+    }
+
+    private void PlayMusic(AudioClip clip)
+    {
+        musicSource.clip = clip;
+        musicSource.Play();
+    }
+
+    public void StopMusic()
+    {
+        musicSource.Stop();
     }
 
     // This method create a smoothly fade out of the sound
