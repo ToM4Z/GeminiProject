00aef9017c56fe618052cb59ac4491fd7e3f9704
Gianfranco Sapia <43842080+Sfrizz97@users.noreply.github.com>
2021-06-15 01:00:59 +0200

    Level 2 90% done

---
+++ Assets/Scripts/Obstacles/PoolBehaviour.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolBehaviour : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider collision) {
+        if(collision.gameObject.tag == "Player") {
+            Debug.Log("SONO ENTRATO NELLA POZZA DI VELENO");
+            collision.GetComponent<PlayerStatisticsController>().hurt(DeathEvent.HITTED,true);
+        }
+    }
+}
