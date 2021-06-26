1bcd6ad2b5d1b67aebaa5e8046241b9c26a89ae9
Thomas <thom_96@hotmail.it>
2021-06-26 01:39:23 +0100

    Added dialogues in hub and level 1

--- Assets/Scripts/DialogueTrigger.cs
+++ Assets/Scripts/DialogueTrigger.cs
@@ -5,10 +5,20 @@ using UnityEngine;
 public class DialogueTrigger : MonoBehaviour
 {
     public int DialogueStart, DialogueEnd;
+    public int condition = 0;
+
+    private bool IsConditionTrue()
+    {
+        switch (condition)
+        {
+            case 1: return GlobalVariables.scores.Keys.Count == 3;
+            default: return true;
+        }
+    }
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && IsConditionTrue())
         {
             DialogueUI.instance.StartDialogue(DialogueStart, DialogueEnd);
             Destroy(this);
